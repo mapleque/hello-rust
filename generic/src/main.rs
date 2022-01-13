c1d8c2f183fab5c2553e9daf86eac1e7332434a7
yangyang <yangyang6@sensetime.com>
2022-01-13 17:02:02 +0800

    feat: generic

---
+++ generic/src/main.rs
@@ -0,0 +1,28 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+fn main() {
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    let p4 = Point { x: 3.0, y: 4.0 };
+    println!("p4 dis to origin is {}", p4.distance_from_origin());
+}
